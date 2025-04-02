9bc73d399ee8910304c77613ac86874cf70cf907
Maki <felipemhtrabalho@gmail.com>
2025-04-02 16:41:14 -0300

    Projeto 07 terminado
    Obs: Falta colocar um hover nos botoes depois de clickado

---
+++ projeto-07/script/script.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll(".btn");
+let selectedNumber = document.querySelector("#selected-number");
+
+document.addEventListener("click", function (event) {
+
+    if (event.target.classList.contains("btn")) {
+
+        buttons.forEach((button) => {
+            button.style.backgroundColor = "hsl(213, 19%, 18%)";
+            button.style.color = "hsl(217, 12%, 63%)";
+            button.classList.add("btn:hover");
+        });
+        const button = event.target;
+        button.style.backgroundColor = "hsl(25, 97%, 53%)";
+        button.style.color = "hsl(216, 12%, 8%)";
+        const buttonText = button.textContent;
+
+        selectedNumber.textContent = buttonText;
+    }
+    
+    if (event.target.classList.contains("btn-btn-submit")) { 
+        document.querySelector(".header").style.display = "none";
+        document.querySelector(".container-star").style.display = "none";
+        document.querySelector(".btns").style.display = "none";
+        document.querySelector(".container-btn").style.display = "none";
+        document.querySelector(".hidden-container").style.display = "block";
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
